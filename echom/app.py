008e020688bc05ec04c2b127dc5a22446b0dd271
Snegakutty <132530737+Snegakutty@users.noreply.github.com>
2024-04-17 17:21:42 +0530

    Update app.py

--- echom/app.py
+++ echom/app.py
@@ -15,7 +15,7 @@ alert_message = "Accident happened to the person at Latitude: 13.0449408 Longitu
 for number in friend_numbers:
     message = client.messages.create(
         body=alert_message,
-        from_='+17792464076',  # Twilio phone number
+        from_='',  # Twilio phone number
         to=number
     )
     print(f"Alert message sent to {number}: {message.sid}")
