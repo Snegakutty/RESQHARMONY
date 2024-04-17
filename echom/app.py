1231f48942799a573279271ecef13cd55d8234c8
Snegakutty <132530737+Snegakutty@users.noreply.github.com>
2024-04-17 17:21:17 +0530

    Update app.py

--- echom/app.py
+++ echom/app.py
@@ -1,12 +1,12 @@
 from twilio.rest import Client
 
 # Your Twilio account SID and auth token
-account_sid = 'AC5531ef9202bb75912c06228c9d3758db'
-auth_token = '007eddf9e7b0efbaa63336fa1aff9c75'
+account_sid = ''
+auth_token = ''
 client = Client(account_sid, auth_token)
 
 # Define the phone numbers you want to send alerts to
-friend_numbers = ['+916382095224', '+918637677387', '+918778405645', '+919585752915']
+friend_numbers = ['']
 
 # Define your alert message
 alert_message = "Accident happened to the person at Latitude: 13.0449408 Longitude: 80.19968"
